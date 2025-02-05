bb9ef9e56389fbe2813ae2f880b29b9d1b22238c
Gaurav Thakare <thakaregaurav7131@gmail.com>
2025-02-05 14:34:46 +0530

    initial commit

---
+++ CSS/Assigment/nw.js
@@ -0,0 +1,17 @@
+<div class="qns">
+                <div>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam placeat hic repella</h4>
+                    <p class="hide">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Labore eos cum fugiat corrupti dignissimos! Ut maiores dolores
+                        consequuntur modi velit quo beatae voluptatibus accusamus neque, qui ducimus animi, nostrum
+                        vero?
+                    </p>
+
+                </div>
+                <div class="add">
+                    <i class="fa-sharp fa-solid fa-plus"></i>
+                    <i class="fa-sharp fa-solid fa-minus hide"></i>
+                </div>
+                </div>
+                
